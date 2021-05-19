15d585c487c20b7fd551380d62598f4355add30e
Stefan Moß <mossstef@gmail.com>
2021-05-19 10:32:00 +0200

    update Qt gui

---
+++ gui.py
@@ -0,0 +1,38 @@
+import sys
+import random
+import os
+import PySide6
+
+dirname = os.path.dirname(PySide6.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+from PySide6 import QtCore, QtWidgets, QtGui
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo"]
+
+        self.button = QtWidgets.QPushButton("click me!")
+        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800,600)
+    widget.show()
+
+    sys.exit(app.exec_())
